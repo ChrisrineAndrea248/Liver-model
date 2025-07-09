cdb5c5aae50b45564d311c688b990fda32dcbb73
ChristineAndrea248 <lojongchristine248@gmail.com>
2025-07-09 19:28:02 +0200

    fix app and requirements

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
+import os
+
 
 app = Flask(__name__)
 
@@ -30,3 +32,6 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
